4ae7fb3c32374786f83ff56ed9abbe1d209a5b59
Maria Luiza Ribas <luuiza.ribass@gmail.com>
2024-06-13 15:53:18 -0300

    .
    .

---
+++ 02.sql
@@ -0,0 +1,39 @@
+/*CREATE DATABASE biblioteca*/
+/*use biblioteca*/
+
+/*
+CREATE TABLE livro (
+ISBN int(13),
+Titulo varchar(30),
+Ano int(4),
+Editora varchar(30),
+Autores varchar(30)
+);
+*/
+
+/*INSERT INTO livro (ISBN, Titulo, Ano, Editora, Autores)
+VALUES(858057301, "Extraordinário", 2013, "Intrínseca", "R. J. Palacio")*/
+
+/*SELECT * FROM livro*/
+
+/*
+CREATE TABLE autores (
+Nome varchar(30),
+Nacionalidade varchar(30)
+);
+*/
+
+/*INSERT INTO autores (Nome, Nacionalidade)
+VALUES("Raquel Jaramillo Palacio", "Americana")*/
+
+/*SELECT * FROM autores*/
+
+/*CREATE TABLE categorias (
+Codigo int(9),
+Descrição varchar(80)
+);*/
+
+/*INSERT INTO categorias (Codigo, Descrição)
+VALUES(963852741, "Extraordinario")*/
+
+/*SELECT * FROM categorias*/
---
+++ 03.sql
@@ -0,0 +1,37 @@
+/*CREATE DATABASE firma*/
+/*use firma*/
+
+/*CREATE TABLE produtos (
+Codigo int(9),
+Nome varchar (30),
+Categoria varchar(50),
+Preço int(5)
+);*/
+
+/*INSERT INTO produtos (Codigo, Nome, Categoria, Preço)
+VALUES(987654321, "Veja", "Detergente", 10.00)*/
+
+/*SELECT * FROM produtos*/
+
+/*CREATE TABLE clientes (
+Codigo int(9),
+Nome varchar (30),
+Endereço varchar(50),
+Telefone int(9),
+Statuss varchar(10)
+);*/
+
+/*INSERT INTO clientes (Codigo, Nome, Endereço, Telefone, Statuss)
+VALUES(321654987, "Mario", "Rua Getulio Vargas", 159263487, "Medio")*/
+
+/*SELECT * FROM clientes*/
+
+/*CREATE TABLE pedidos (
+Numeros int(9),
+Datas int(10)
+);*/
+
+/*INSERT INTO pedidos (Numeros, Datas)
+VALUES (357689241, 23/08/23)*/
+
+/*SELECT * FROM pedidos*/
